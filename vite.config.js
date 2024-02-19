7f21445d3b0187154710feb37f44161d7e2239b1
javi <javis9555@gmail.com>
2024-02-19 16:16:40 +0100

    variables de .config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Cargar las variables de entorno
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      // Exponer las variables de entorno
+      "import.meta.env": JSON.stringify(env),
+    },
+  };
+});
