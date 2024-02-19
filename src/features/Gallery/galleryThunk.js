e43ddbc535d0882c8d6bd0bafe91c3739fbc2a52
javi <javis9555@gmail.com>
2024-02-19 16:09:01 +0100

    clg

--- src/features/Gallery/galleryThunk.js
+++ src/features/Gallery/galleryThunk.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const key = import.meta.env.VITE_VITE_API_KEY;
-
+console.log(key);
 export const galleryThunk = createAsyncThunk(
   "search/galleryThunck",
   async (look, page) => {
